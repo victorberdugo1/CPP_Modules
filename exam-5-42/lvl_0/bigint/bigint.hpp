7c2de717f2378859b9255feb984aee75d8e809c7
Victor <victor@localhost.localdomain>
2025-10-17 21:00:06 +0200

    imp

--- exam-5-42/lvl_0/bigint/bigint.hpp
+++ exam-5-42/lvl_0/bigint/bigint.hpp
@@ -1,137 +1,97 @@
-﻿#ifndef BIGINT_HPP
+#ifndef BIGINT_HPP
 #define BIGINT_HPP
 #include <iostream>
 #include <string>
 #include <algorithm>
 #include <climits>
-class bigint {
-public:
+class bigint{
+private:
 	std::string _val;
-	bigint() : _val("0") {}
+public:
+	bigint() : _val("0"){}
 	bigint(unsigned long long n) : _val(std::to_string(n)) {}
-	bigint(const std::string& s) : _val(s.empty() ? "0" : s) {
+	bigint(const std::string& s) : _val(s.empty() ? "0" : s)  {
 		normalize();
 	}
 	void normalize() {
 		size_t pos = _val.find_first_not_of('0');
 		_val = (pos == std::string::npos) ? "0" : _val.substr(pos);
 	}
-	unsigned int toUInt() const {
-		unsigned long long acc = 0;
-		for (char c : _val) {
-			if ((acc = acc * 10 + (c - '0')) > UINT_MAX) {
-				return UINT_MAX;
-			}
-		}
-		return acc;
+	friend std::ostream& operator<<(std::ostream& os, const bigint& n) {
+		return os << n._val;
 	}
-	bigint operator+(const bigint& rhs) const {
+	bigint operator+(const bigint& rhs) const{
 		std::string res;
-		int i = _val.size() - 1, j = rhs._val.size() - 1, carry = 0;
-		while (i >= 0 || j >= 0 || carry) {
+		int i = _val.size() - 1, j = rhs._val.size() -1, carry = 0;
+		while(i >= 0 || j >= 0 || carry){
 			int sum = carry;
-			if (i >= 0)
-				sum += _val[i--] - '0';
-			if (j >= 0)
-				sum += rhs._val[j--] - '0';
+			if (i >= 0) sum += _val[i--] - '0';
+			if (j >= 0) sum += rhs._val[j--] - '0';
 			res += '0' + (sum % 10);
 			carry = sum / 10;
 		}
-		std::reverse(res.begin(), res.end());
+		std::reverse(res.begin(),res.end());
 		return bigint(res);
 	}
-	bool operator<(const bigint& o) const {
+	bool operator<(const bigint& o) const{
 		if (_val.size() != o._val.size())
 			return _val.size() < o._val.size();
 		return _val < o._val;
 	}
-	bigint operator<<(int shift) const {
-		if (shift <= 0) return *this;
-		return bigint(_val + std::string(shift, '0'));
+	bool operator>(const bigint& o) const {
+		return o < *this;
 	}
-	bigint operator>>(unsigned int shift) const {
-		if (shift == 0 || _val == "0") return *this;
-		if (shift >= _val.size()) return bigint(0);
-		return bigint(_val.substr(0, _val.size() - shift));
+	bool operator>=(const bigint& o) const {
+		return !(*this < o);
+	}
+	bool operator<=(const bigint& o) const {
+		return !(*this > o);
+	}
+	bool operator!=(const bigint& o) const {
+		return !(*this == o);
 	}
 	bool operator==(const bigint& o) const {
 		return _val == o._val;
 	}
-	
-	friend std::ostream& operator<<(std::ostream& os, const bigint& n) {
-		return os << n._val;
+	bigint operator<<(int shift) const {
+		if (shift <= 0 || _val == "0")
+			return *this;
+		return bigint(_val + std::string(shift, '0'));
 	}
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// COMPARISONS: Define < and ==, the rest are negations/inversions
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-	// != is simply the negation of ==
-	bool operator!=(const bigint& o) const {
-		return !(*this == o);
+	bigint operator>>(unsigned int shift) const {
+		if (shift == 0 || _val == "0")
+			return *this;
+		if (shift >= _val.size()) return bigint(0);
+		return bigint(_val.substr(0, _val.size() - shift));
 	}
-	// > swaps the operands: a > b is the same as b < a
-	bool operator>(const bigint& o) const {
-		return o < *this;
-	}
-	// <= is the negation of >: if it's not greater, it's less or equal
-	bool operator<=(const bigint& o) const {
-		return !(*this > o);
-	}
-	// >= is the negation of <: if it's not less, it's greater or equal
-	bool operator>=(const bigint& o) const {
-		return !(*this < o);
-	}
-
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// Pattern *this = *this [operator] value
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-	// a += b  →  a = a + b
-	bigint& operator+=(const bigint& rhs) {
-		return *this = *this + rhs;
-	}
-	// a <<= n  →  a = a << n
-	bigint& operator<<=(int shift) {
-		return *this = *this << shift;
-	}
-	//  >>= n  →  a = a >> n
 	bigint& operator>>=(int shift) {
 		return *this = *this >> shift;
-	}
-	// Versions with bigint: convert to uint and use the int version
-	bigint& operator<<=(const bigint& shift) {
-		return *this = *this << shift;
 	}
 	bigint& operator>>=(const bigint& shift) {
 		return *this = *this >> shift;
 	}
-
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// INCREMENT: ++a returns the new value, a++ returns the old value
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// Pre-increment (++a): increments and returns the new value
+	bigint& operator<<=(int shift) {
+		return *this = *this << shift;
+	}
 	bigint& operator++() {
 		return *this += bigint(1);
 	}
-	// Post-increment (a++): returns the old value, then increments
-	// The (int) is fictitious, only to differentiate
 	bigint operator++(int) {
-		bigint tmp(*this);  // Save current value
-		++*this;            // Increment using pre-increment
-		return tmp;         // Return the old value
+		bigint tmp(*this);
+		++*this;
+		return tmp;
 	}
-
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// OVERLOADS: Convert bigint to int and reuse existing operators
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// Allows using bigint as a shift amount
+	bigint& operator+=(const bigint& rhs) {
+		return *this = *this + rhs;
+	}
 	bigint operator<<(const bigint& shift) const {
-		return *this << shift.toUInt();
+		unsigned long val = std::min(std::stoul(shift._val), (unsigned long)INT_MAX);
+		return *this << static_cast<int>(val);
 	}
 	bigint operator>>(const bigint& shift) const {
-		return *this >> shift.toUInt();
+		unsigned long val = std::min(std::stoul(shift._val), (unsigned long)INT_MAX);
+		return *this >> static_cast<unsigned int>(val);
 	}
-
 	bigint operator-(const bigint&) const { return bigint(0); }
 };
 #endif
--- exam-5-42/lvl_0/vect2/vect2.hpp
+++ exam-5-42/lvl_0/vect2/vect2.hpp
@@ -1,118 +1,90 @@
 #ifndef VECT2_HPP
 #define VECT2_HPP
 #include <iostream>
-
-// Vector 2D con enteros
 class vect2 {
 private:
-	int x, y;
-
+	int x;int y;
 public:
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// CONSTRUCTORES
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	vect2() : x(0), y(0) {}
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// CONSTRUCTORES
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+	vect2() : x(0),y(0) {}
 	vect2(int x, int y) : x(x), y(y) {}
-
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// ACCESO A COMPONENTES: [] (const y no-const)
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ACCESO A COMPONENTES: [] (const y no-const)
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 	int operator[](int index) const {
-		return (index == 0) ? x : y;
+		return (index == 0) ? x : y ; 
 	}
 	int& operator[](int index) {
-		return (index == 0) ? x : y;
+		return (index == 0) ? x : y ; 
+	}
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// SALIDA: operador 
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+	friend std::ostream& operator<<(std::ostream& os,const vect2& v){
+		return os << "{" << v[0] << ", " << v[1] << "}";
+	}
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// OPERACIONES
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+	bool operator!=(const vect2 v) {
+		return x != v.x || y != v.y;
+	}
+	bool operator==(const vect2 v) {
+		return x == v.x && y == v.y;
 	}
 
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// OPERACIONES VECTORIALES: + - (binarios)
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	vect2 operator+(const vect2& o) const {
-		return vect2(x + o.x, y + o.y);
-	}
-	vect2 operator-(const vect2& o) const {
-		return vect2(x - o.x, y - o.y);
-	}
-
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// OPERADOR UNARIO: negativo (-)
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 	vect2 operator-() const {
 		return vect2(-x, -y);
 	}
+	vect2 operator-(const vect2& o) const{
+		return vect2(x - o.x, y - o.y);
+	}
 
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// MULTIPLICACIÓN POR ESCALAR: * (ambas direcciones)
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	vect2 operator*(int n) const {
+	vect2 operator+(const vect2& o) const{
+		return vect2(x + o.x, y + o.y);
+	}
+
+	vect2 operator*(int n) const{
 		return vect2(x * n, y * n);
 	}
-	friend inline vect2 operator*(int n, const vect2& v) {
+	friend inline vect2 operator*(int n,const vect2& v){
 		return v * n;
 	}
-
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// COMPARACIONES: == y !=
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	bool operator==(const vect2& o) const {
-		return x == o.x && y == o.y;
-	}
-	bool operator!=(const vect2& o) const {
-		return x != o.x || y != o.y;
-	}
-
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// OPERADORES COMPUESTOS: += -= *=
-	// Patrón: *this = *this [operador] valor
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	vect2& operator+=(const vect2& o) {
-		x += o.x; 
-		y += o.y;
-		return *this;
-	}
-	vect2& operator-=(const vect2& o) {
-		x -= o.x; 
-		y -= o.y;
-		return *this;
-	}
 	vect2& operator*=(int n) {
-		x *= n; 
+		x *= n;
 		y *= n;
 		return *this;
 	}
 
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// INCREMENTO/DECREMENTO: ++/-- (pre y post)
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// Pre-incremento (++v): incrementa y retorna el nuevo valor
-	vect2& operator++() {
-		++x; ++y;
+	vect2& operator+=(const vect2 v ) {
+		x += v.x;
+		y += v.y;
 		return *this;
 	}
-	// Post-incremento (v++): retorna el viejo valor, luego incrementa
-	vect2 operator++(int) {
-		vect2 tmp(*this);
-		++*this;
-		return tmp;
+	vect2& operator-=(const vect2 v ) {
+		x -= v.x;
+		y -= v.y;
+		return *this;
 	}
-	// Pre-decremento (--v): decrementa y retorna el nuevo valor
 	vect2& operator--() {
-		--x; --y;
+		--x;--y;
 		return *this;
 	}
-	// Post-decremento (v--): retorna el viejo valor, luego decrementa
 	vect2 operator--(int) {
-		vect2 tmp(*this);
+		vect2 temp(*this);
 		--*this;
-		return tmp;
+		return temp;
 	}
-
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	// SALIDA: operador 
-	// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-	friend std::ostream& operator<<(std::ostream& os, const vect2& v) {
-		return os << "{" << v[0] << ", " << v[1] << "}";
+	vect2& operator++() {
+		++x;++y;
+		return *this;
+	}
+	vect2 operator++(int) {
+		vect2 temp(*this);
+		++*this;
+		return temp;
 	}
 };
-
-#endif // VECT2_HPP
+#endif
